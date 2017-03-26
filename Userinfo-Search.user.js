83f2a224e8d7da1cc86041521831bea1c484a981
Rikard <rikardhypixel@gmail.com>
2017-03-26 14:44:26 +0200

    Update Userinfo-Search.user.js

--- Userinfo-Search.user.js
+++ Userinfo-Search.user.js
@@ -1,4 +1,3 @@
-
 // ==UserScript==
 // @name         Userinfo Search
 // @namespace    http://tampermonkey.net/
@@ -10,3 +9,4 @@
 // @include     https://goliath.hypixel.net/userinfo#*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
+var names = [];
