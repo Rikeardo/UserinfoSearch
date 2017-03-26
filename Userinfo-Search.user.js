ed3828191ba6bc01bcb469c3be0ef011804319e5
Rikard <rikardhypixel@gmail.com>
2017-03-26 14:43:48 +0200

    Create Userinfo-Search.user.js

--- Userinfo-Search.user.js
+++ Userinfo-Search.user.js
@@ -1,1 +1,12 @@
 
+// ==UserScript==
+// @name         Userinfo Search
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Displaying names differently
+// @author       _Rikardo_
+// @include     https://goliath.hypixel.net/userinfo
+// @include     https://goliath.hypixel.net/userinfo?*
+// @include     https://goliath.hypixel.net/userinfo#*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// ==/UserScript==
