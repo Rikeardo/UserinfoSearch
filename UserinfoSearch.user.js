cfd75db1d31887be7fe4157d4e63ed2678ab5a24
Rikard <rikardhypixel@gmail.com>
2017-03-26 15:08:07 +0200

    Update UserinfoSearch.user.js

--- UserinfoSearch.user.js
+++ UserinfoSearch.user.js
@@ -1,6 +1,5 @@
 // ==UserScript==
 // @name         Userinfo Search
-// @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Displaying names differently
 // @author       _Rikardo_
